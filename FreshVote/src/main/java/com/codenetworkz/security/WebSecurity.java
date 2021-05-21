e8f03c3857c0e469855fe09080ed28418b7beec3
robindhole2990 <robindhole2990.rd@gmail.com>
2021-05-21 16:28:36 +0530

    Custom Login page

--- FreshVote/src/main/java/com/codenetworkz/security/WebSecurity.java
+++ FreshVote/src/main/java/com/codenetworkz/security/WebSecurity.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
+@EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter{
 	
 	@Bean
@@ -20,13 +21,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("robindhole@gmail.com").password(getPasswordEncoder().encode("redhat")).roles("USER");
+		auth.inMemoryAuthentication()
+			.withUser("robindhole@gmail.com")
+			.password(getPasswordEncoder().encode("redhat"))
+			.roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
-			.authorizeRequests().antMatchers("/").permitAll()
+			
+			.authorizeRequests()
+			.antMatchers("/", "/dashboardfile/**").permitAll()
 			.anyRequest().hasRole("USER").and()
 			.formLogin()
 				.loginPage("/login").permitAll()
