92f29b41c42b2ab1a4013f7976dfd7b12f7c01a6
robindhole2990 <robindhole2990.rd@gmail.com>
2021-05-21 13:10:47 +0530

    initial commit

---
+++ FreshVote/src/main/java/com/codenetworkz/FreshVoteApplication.java
@@ -0,0 +1,13 @@
+package com.codenetworkz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshVoteApplication.class, args);
+	}
+
+}
---
+++ FreshVote/src/test/java/com/codenetworkz/FreshVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenetworkz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
