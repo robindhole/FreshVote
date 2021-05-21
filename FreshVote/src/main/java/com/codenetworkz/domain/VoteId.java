dfc4142927cb62ae5888d6cc5350568ee61e99bf
robindhole2990 <robindhole2990.rd@gmail.com>
2021-05-21 14:11:09 +0530

    Entities

---
+++ FreshVote/src/main/java/com/codenetworkz/domain/VoteId.java
@@ -0,0 +1,40 @@
+package com.codenetworkz.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class VoteId implements Serializable{
+
+	
+
+	private static final long serialVersionUID = -6897471281125367684L;
+
+	@ManyToOne
+	private User user;
+	
+	@ManyToOne
+	private Feature feature;
+
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	public Feature getFeature() {
+		return feature;
+	}
+
+	public void setFeature(Feature feature) {
+		this.feature = feature;
+	}
+	
+	
+}
