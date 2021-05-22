65acb53c4b61b5be5c0460bfac7247ebd6b0ccc4
robindhole2990 <robindhole2990.rd@gmail.com>
2021-05-22 07:44:44 +0530

    enable csrf token

--- FreshVote/src/main/java/com/codenetworkz/security/WebSecurity.java
+++ FreshVote/src/main/java/com/codenetworkz/security/WebSecurity.java
@@ -29,7 +29,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http
 			
 			.authorizeRequests()
 			.antMatchers("/", "/dashboardfile/**", "/register", "/forgot-password").permitAll()
